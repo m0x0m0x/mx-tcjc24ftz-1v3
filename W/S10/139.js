ff1ea0f75d264edbc78e8641ecb267fdef9b6a06
m0x0m0x <mx0mx@tuta.io>
2024-03-21 03:45:11 +0530

    140 coding challenge

--- W/S10/139.js
+++ W/S10/139.js
@@ -17,6 +17,39 @@ const g = function () {
   };
 };
 
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
 // If you dont call g() , then f() will return error
 g();
 f();
+console.dir(f);
+
+// Reassigned f
+h();
+f();
+console.dir(f);
+
+subHead("Closure Example 2");
+const boardPass = function (n, wait) {
+  const perGr = n / 3;
+
+  setTimeout(function () {
+    console.log(`Now Boarding a ${n}`);
+    console.log(`Three groups each with ${perGr} passengers`);
+  }, wait * 1000);
+
+  console.log(`Start Boarding in ${wait} seconds`);
+};
+
+const perGr = 1000;
+boardPass(180, 3);
+
+// Timer Function
+// setTimeout(function () {
+//   console.log(`TIMER `);
+// }, 2000);
---
+++ W/S10/140c2.js
@@ -0,0 +1,8 @@
+"use strict";
+/* 
+140: Coding Challenge 2 
+*/
+// === import zone ===
+import { mainHead, subHead, paraText } from "../../utils/text.js";
+// ======
+mainHead("140: Coding Challenge 2 ");
