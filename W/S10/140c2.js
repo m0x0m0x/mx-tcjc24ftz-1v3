e3042f05b42fefbfb4542078f0702e26b360f7af
m0x0m0x <mx0mx@tuta.io>
2024-03-21 04:22:28 +0530

    asd

--- W/S10/140c2.js
+++ W/S10/140c2.js
@@ -6,3 +6,12 @@
 import { mainHead, subHead, paraText } from "../../utils/text.js";
 // ======
 mainHead("140: Coding Challenge 2 ");
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+
+  document.querySelector("body").addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
