420eacff6f0ab3f458c066704bbc9699ac73e658
m0x0m0x <mx0mx@tuta.io>
2024-03-20 23:26:56 +0530

    138

--- W/S10/138.js
+++ W/S10/138.js
@@ -6,3 +6,23 @@
 import { mainHead, subHead, paraText } from "../../utils/text.js";
 // ======
 mainHead("138: Closures");
+
+subHead("Closures testing");
+
+//closures here
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengersz`);
+  };
+};
+
+const booker = secureBooking();
+console.log(booker);
+
+paraText("Calling Function");
+booker();
+booker();
+booker();
