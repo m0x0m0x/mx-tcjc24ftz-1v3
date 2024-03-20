92b22c18eeeee7dac64d2df91670911b73687fc1
m0x0m0x <mx0mx@tuta.io>
2024-03-20 22:47:57 +0530

    ..

--- W/S10/137.js
+++ W/S10/137.js
@@ -6,3 +6,21 @@
 import { mainHead, subHead, paraText } from "../../utils/text.js";
 // ======
 mainHead("137: Immediately Invoked Function Expressions - IIFE");
+
+subHead(`
+IIFE - Functions that are invoked only once 
+`);
+
+// Example of running it once
+const runOnce = function () {
+  console.log("Run One Ony");
+};
+// runOnce();
+
+// This is being called immediately
+(function () {
+  console.log("Run One Ony");
+})();
+
+// Running an arrow function
+(() => console.log("Run once"))();
